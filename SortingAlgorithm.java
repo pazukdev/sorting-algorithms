31b787d545cb65ed55c1ee513e362209411f20b8
pazukdev <pazukdev@gmail.com>
2018-03-14 12:19:58 +0300

    Add files via upload

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by Pazuk on 09.03.2018.
@@ -8,12 +9,15 @@ public class SortingAlgorithm {
     //static int[] array = {4, 0, 2, 10, 5, 7, 9, 8, 3, 1, 6};
 
     public static void main(String[] args) {
-        int[] array = {6, 4, 0, 2, 10, 5, 6, 7, 9, 8, 3, 1, 6, 6, 6};
+
+        //int[] array=randomArray(30);
+        //int[] array = {6, 4, 0, 2, 10, 5, 6, 7, 9, 8, 3, 1, 6, 6, 6};
+        int[] array={2, 1, 3, 4};
         System.out.println("Unsorted array:");
         System.out.println(Arrays.toString(array));
 
         // Select sort algorithm:
-        //bubbleSort(array);
+        bubbleSort(array);
         //bubbleSort1(array);
         //gnomeSort(array);
         //selectionSort(array);
@@ -21,9 +25,23 @@ public class SortingAlgorithm {
         //combSort(array);
         //quickSort(array);
         //quickSort2(array);
-        quickSort3Builtin(array);
+        //quickSort3Builtin(array);
+        //mergeSort(array); // merge sort code is in developing...
 
     }
+
+    static int[] randomArray(int arrayLength){
+        int[] array=new int[arrayLength];
+        Random random=new Random();
+        for(int i=0; i<array.length; i++){
+            array[i]=random.nextInt(100);
+        }
+        return array;
+    }
+
+
+
+
 
     static void bubbleSort(int[] array){
         boolean isThereUnsortedElements=true;
@@ -183,7 +201,6 @@ public class SortingAlgorithm {
         int i=low;
         int j=low;
         int pivot=high;
-        System.out.println("Pivot: "+array[pivot]);
 
         while(j<pivot){
             if(array[j]<array[pivot]){
@@ -213,6 +230,109 @@ public class SortingAlgorithm {
 
         System.out.println("Quick sort 3, built-in:");
         System.out.println(Arrays.toString(array));
+    }
+
+    // end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // merge sort code is in developing...
+
+    static void mergeSort(int[] array){
+        int low=0;
+        int high=array.length-1;
+        //array=mergeSortAlgorithm(array, low, high);
+        mergeSort3(array, low, high);
+
+        System.out.println("Merge sort:");
+        System.out.println(Arrays.toString(array));
+    }
+
+    static int[] mergeSortAlgorithm(int[] array, int low, int high){
+        //if(low==high) return array;
+
+        int mid=(low+high)/2;
+        //System.out.println(array[mid]);
+        mergeSortAlgorithm(array, low, mid);
+        mergeSortAlgorithm(array, mid+1, high);
+        merge(array, low, mid, high);
+
+        return array;
+    }
+
+    static void merge(int[] array, int low, int mid, int high){
+        int[] left=new int[mid-low+2];
+        for(int i=low; i<=mid; i++){
+            left[i-low]=array[i];
+        }
+        System.out.println("left: "+Arrays.toString(left));
+        left[mid-low+1]=Integer.MAX_VALUE;
+        System.out.println("left with MAX_VALUE: "+Arrays.toString(left));
+        int[] right=new int[high-mid+1];
+        for(int i=mid+1; i<=high; i++){
+            right[i-mid-1]=array[i];
+        }
+        System.out.println("right: "+Arrays.toString(right));
+        right[high-mid]=Integer.MAX_VALUE;
+        System.out.println("right with MAX_VALUE: "+Arrays.toString(right));
+        int i=0;
+        int j=0;
+        for(int k=low; k<=high; k++){
+            if(left[i]<=right[j]){
+                array[k]=left[i];
+                i++;
+            } else {
+                array[k]=right[j];
+                j++;
+            }
+        }
+
+    }
+
+    public static void mergeSort3(int[] array, int low, int high) {
+        if (low + 1 < high) {
+            int mid = (low + high)/2;
+            //System.out.println("[mid]: "+mid);
+            //System.out.println("mid: "+array[mid]);
+            mergeSort3(array, low, mid);
+            mergeSort3(array, mid, high);
+
+            int size = high - low;
+            System.out.println("size: "+size);
+            int[] tempArray = new int[size];
+            int i = low;
+            int j = mid;
+            for (int k = 0; k < size; k++) {
+                if (j >= high || i < mid && array[i] < array[j]) {
+                    tempArray[k] = array[i++];
+                } else {
+                    tempArray[k] = array[j++];
+                }
+            }
+            System.arraycopy(tempArray, 0, array, low, size);
+        }
+
+        //System.out.println("Merge sort:");
+        //System.out.println(Arrays.toString(array));
+
     }
 
 
